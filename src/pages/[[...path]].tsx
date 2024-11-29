e1d3b3f899e7d37fd60c7fa34f5537e1364065cd
azar-altudo <azar.safarov@altudo.co>
2024-11-29 11:50:24 -0500

    dynamic route added

--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,3 +1,5 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { ParsedUrlQuery } from 'querystring';
 import localFont from 'next/font/local';
 import { client } from '@/sanity/lib/client';
 
@@ -15,10 +17,15 @@ const geistMono = localFont({
 type Post = {
   _id: string;
   title: string;
+  path?: string[];
   // Add other fields you have in your Sanity schema
 };
 
-export default function Home(props: Readonly<{ posts: Post[] }>) {
+interface IParams extends ParsedUrlQuery {
+  path?: string[];
+}
+
+export default function Page(props: Readonly<{ posts: Post[] }>) {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -40,14 +47,34 @@ export default function Home(props: Readonly<{ posts: Post[] }>) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Hardcoded paths for now
+  const paths = [
+    { params: { path: [] } }, // "/"
+    { params: { path: ['en'] } }, // "/en"
+    { params: { path: ['en', 'blog-1'] } }, // "/en/blog-1"
+    { params: { path: ['blog-1'] } }, // "/blog-1"
+  ];
+
+  return {
+    paths,
+    fallback: false, // Set to 'blocking' if you want to generate new paths on demand
+  };
+};
+
+export const getStaticProps: GetStaticProps<
+  { posts: Post[]; path: string[] },
+  IParams
+> = async ({ params }) => {
+  const path = params?.path || [];
   const query = `*[_type == "post"]`;
   const posts = await client.fetch(query);
 
   return {
     props: {
       posts,
+      path,
     },
     revalidate: 1800, // 30 minutes in seconds
   };
-}
+};
