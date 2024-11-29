6ea1ec14db572bd805ae947e34b871f22c9f83d2
azar-altudo <azar.safarov@altudo.co>
2024-11-29 11:19:30 -0500

    Data fetching updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import localFont from 'next/font/local';
 import { client } from '@/sanity/lib/client';
-import { useState, useEffect } from 'react';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -19,19 +18,7 @@ type Post = {
   // Add other fields you have in your Sanity schema
 };
 
-export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const query = `*[_type == "post"]`; // Modify this query based on your schema
-      const result = await client.fetch(query);
-      setPosts(result);
-    };
-
-    fetchPosts();
-  }, []);
-
+export default function Home(props: Readonly<{ posts: Post[] }>) {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -40,7 +27,7 @@ export default function Home() {
         <div className="w-full">
           <h2 className="text-xl font-bold mb-4">Posts</h2>
           <ul className="space-y-4">
-            {posts.map((post) => (
+            {props.posts.map((post) => (
               <li key={post._id} className="p-4 border rounded">
                 <h3 className="font-bold">{post.title}</h3>
                 {/* Add other post fields you want to display */}
